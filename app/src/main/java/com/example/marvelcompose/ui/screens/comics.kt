8bb2216919becd0c50f18dc702568e9a66ea07a2
ernestschneider <ernestschneider@gmail.com>
2023-02-21 12:15:58 +0100

    Addeed tabs to comics

--- app/src/main/java/com/example/marvelcompose/ui/screens/comics.kt
+++ app/src/main/java/com/example/marvelcompose/ui/screens/comics.kt
@@ -2,23 +2,20 @@ package com.example.marvelcompose.ui.screens
 
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import coil.annotation.ExperimentalCoilApi
-import com.example.marvelcompose.R
 import com.example.marvelcompose.data.entities.Comic
 import com.example.marvelcompose.data.repositories.ComicsRepository
 import com.example.marvelcompose.ui.screens.common.MarvelItemDetailScreen
 import com.example.marvelcompose.ui.screens.common.MarvelItemsList
-import com.example.marvelcompose.ui.screens.common.MarvelItemsListScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @ExperimentalCoilApi
@@ -30,15 +27,40 @@ fun ComicsScreen(onClick: (Comic) -> Unit) {
         comicsState = ComicsRepository.get()
     }
 
-    val formats = Comic.Format.values()
-    HorizontalPager(
-        count = formats.size
-    ) {
-        MarvelItemsList(
-            items = comicsState,
-            onClick = onClick
-        )
+    val formats = Comic.Format.values().take(3)
+    val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
+    
+    Column {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            formats.forEach {
+                Tab(
+                    selected = it.ordinal == pagerState.currentPage,
+                    onClick = { scope.launch { pagerState.scrollToPage(it.ordinal) } },
+                    text = { Text(text = it.name) }
+                )
+
+            }
+        }
+        HorizontalPager(
+            count = formats.size,
+            state = pagerState
+        ) {
+            MarvelItemsList(
+                items = comicsState,
+                onClick = onClick
+            )
+        }
     }
+    
+
 }
 
 @ExperimentalCoilApi
