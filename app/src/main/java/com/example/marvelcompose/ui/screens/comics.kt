38f54706dc85860d82b2b96802abad5ee07cb047
ernestschneider <ernestschneider@gmail.com>
2023-02-23 10:20:22 +0100

    Format comics format names

--- app/src/main/java/com/example/marvelcompose/ui/screens/comics.kt
+++ app/src/main/java/com/example/marvelcompose/ui/screens/comics.kt
@@ -1,13 +1,17 @@
 package com.example.marvelcompose.ui.screens
 
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
+import com.example.marvelcompose.R
 import com.example.marvelcompose.data.entities.Comic
 import com.example.marvelcompose.data.repositories.ComicsRepository
 import com.example.marvelcompose.ui.screens.common.MarvelItemDetailScreen
@@ -66,11 +70,23 @@ private fun ComicFormatsTabRow(
             Tab(
                 selected = it.ordinal == pagerState.currentPage,
                 onClick = { scope.launch { pagerState.scrollToPage(it.ordinal) } },
-                text = { Text(text = it.name) }
+                text = { Text(text = stringResource(id = it.toStringRes()).uppercase() ) }
             )
 
         }
     }
+}
+
+@StringRes
+private fun Comic.Format.toStringRes(): Int = when (this) {
+    Comic.Format.COMIC -> R.string.comic
+    Comic.Format.MAGAZINE -> R.string.magazine
+    Comic.Format.TRADE_PAPERBACK -> R.string.trade_paperback
+    Comic.Format.HARDCOVER -> R.string.hardcover
+    Comic.Format.DIGEST -> R.string.digest
+    Comic.Format.GRAPHIC_NOVEL -> R.string.graphic_novel
+    Comic.Format.DIGITAL_COMIC -> R.string.digital_comic
+    Comic.Format.INFINITE_COMIC -> R.string.infinite_comic
 }
 
 @ExperimentalCoilApi
