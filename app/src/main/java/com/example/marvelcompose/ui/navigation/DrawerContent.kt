d34c572de6c4a07d26e9dcb30064e81fe830ba86
ernestschneider <ernestschneider@gmail.com>
2023-03-31 11:56:58 +0200

    Added Locals providers

--- app/src/main/java/com/example/marvelcompose/ui/navigation/DrawerContent.kt
+++ app/src/main/java/com/example/marvelcompose/ui/navigation/DrawerContent.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -44,34 +42,37 @@ fun DrawerContent(
     drawerOptions.forEachIndexed { index, navItem ->
         val selected = selectedIndex == index
         val colors = MaterialTheme.colors
-
-        Row(modifier = Modifier
-            .clickable { onOptionClick(navItem) }
-            .fillMaxWidth()
-            .padding(8.dp, 4.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(color = if (selected) colors.primary.copy(alpha = 0.12f) else colors.surface)
-            .padding(12.dp)
+        val localContentColor = if (selected) colors.primary else colors.onBackground
+        CompositionLocalProvider(
+            LocalTextStyle provides MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
+            LocalContentColor provides localContentColor,
         ) {
-            Icon(
-                imageVector = navItem.icon,
-                tint = if (selected) colors.primary else colors.onSurface.copy(
-                    alpha = ContentAlpha.medium
-                ),
-                contentDescription = stringResource(id = navItem.title)
-            )
-            Spacer(modifier = Modifier.width(24.dp))
-            Text(
-                text = stringResource(id = navItem.title),
-                style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
-                color = if (selected) colors.primary else colors.onSurface
-            )
+            Row(modifier = Modifier
+                .clickable { onOptionClick(navItem) }
+                .fillMaxWidth()
+                .padding(8.dp, 4.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(color = if (selected) LocalContentColor.current.copy(alpha = 0.12f) else colors.surface)
+                .padding(12.dp)
+            ) {
+                val contentAlpha = if (selected) ContentAlpha.high else ContentAlpha.medium
+                CompositionLocalProvider(LocalContentAlpha provides contentAlpha) {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = stringResource(id = navItem.title)
+                    )
+                }
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(
+                    text = stringResource(id = navItem.title)
+                )
+            }
         }
     }
 }
 
 @Preview
-@Preview(uiMode = UI_MODE_NIGHT_YES )
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DrawerContentPreview() {
     MarvelScreen {
