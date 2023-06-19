a026f6724e7f070e67dce30f77fc4c83bbecc1f1
ernestschneider <ernestschneider@gmail.com>
2023-06-19 09:46:58 +0200

    Playing with colors

--- app/src/main/java/com/example/marvelcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/marvelcompose/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Red = Color(0xFFE23636)
+val RedDark = Color(0xFFA20000)
+val Blue = Color(0xFF518CCA)
