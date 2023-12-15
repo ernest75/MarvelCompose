592c8ac3032df931f1352fa1211707fba547bcbd
ernestschneider <ernestschneider@gmail.com>
2023-12-15 11:37:55 +0100

    Fixed adatpitve grid cell value

--- app/src/main/java/com/example/marvelcompose/ui/screens/common/MarvelItemsListScreen.kt
+++ app/src/main/java/com/example/marvelcompose/ui/screens/common/MarvelItemsListScreen.kt
@@ -1,8 +1,6 @@
 package com.example.marvelcompose.ui.screens.common
 
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.BackHandler
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,12 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import com.example.marvelcompose.data.entities.MarvelItem
@@ -88,7 +84,7 @@ fun <T : MarvelItem> MarvelItemsList(
         }
         if (items.isNotEmpty()) {
             LazyVerticalGrid(
-                columns = GridCells.Adaptive(180.dp),
+                columns = GridCells.Adaptive(170.dp),
                 contentPadding = PaddingValues(4.dp)
             ) {
                 items(items) {
